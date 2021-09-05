e94abd2dc915d75fb00ef1a77baea52dc7be8606
Chinwe Odimmegwa <chynweod@gmail.com>
2021-09-05 15:25:24 +0100

    Add files via upload

---
+++ src/lib/PHPMailer/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Fernico - Ridiculously lite PHP framework
+ *
+ * @author Areeb Majeed, Volcrado Holdings
+ * @package Fernico
+ * @copyright 2017 - Volcrado Holdings Limited
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://volcrado.com/
+ *
+ */
+
+if (!defined('FERNICO')) {
+    fernico_destroy();
+}
+
+require_once("class.phpmailer.php");
+require_once("class.smtp.php");
