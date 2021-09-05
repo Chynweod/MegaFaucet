c22376cde8e8a55fad18b46bd9bbbe162931b401
Chinwe Odimmegwa <chynweod@gmail.com>
2021-09-05 15:01:06 +0100

    Add files via upload

---
+++ src/config/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Fernico - Ridiculously lite PHP framework
+ *
+ * @author Areeb Majeed, Volcrado Holdings
+ * @package Fernico
+ * @copyright 2017 - Volcrado Holdings Limited
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://volcrado.com/
+ *
+ */
+
+define("FERNICO",true);
+
+require('lib/Astrid/init.php');
+new fernico();
